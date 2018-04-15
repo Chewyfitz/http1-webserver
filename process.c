e20aded494fb48aa8e6c8ab112f66e73d8637da3
Aidan Fitzpatrick <a.fitzpatrick1@student.unimelb.edu.au>
2018-04-15 23:31:24 +1000

    fixed gross calloc

--- process.c
+++ process.c
@@ -87,7 +87,8 @@ void sendchar(FILE* file, int sock){
 		printf("%c", just_read);
 	}
 	send_buff[i] = '\0';
-	char* concat_message = calloc(0, (strlen(http_message) + strlen(send_buff) + 1) * sizeof(char));
+	char* concat_message = malloc((strlen(http_message) + send_buff_size + 1) * sizeof(char));
+	concat_message[0] = '\0';
 	strcat(concat_message, http_message);
 	strcat(concat_message, send_buff);
 	printf("\n");

d0c15bd88baefe17c59adc0f7761b01f0f2a9ca6
Aidan Fitzpatrick <a.fitzpatrick1@student.unimelb.edu.au>
2018-04-15 18:08:54 +1000

    getExt should work now

--- process.c
+++ process.c
@@ -31,7 +31,7 @@ void getExt(char* extension, char* filename){
 	int i = 0;
 	while((filename[len - (i++)] != '.') && len - i > 0) printf("2: %c\n", filename[len - (i)]);
 
-	strcpy(extension, filename + i);
+	strcpy(extension, filename + (len - i));
 	printf("3: %s\n", extension);
 }
 
